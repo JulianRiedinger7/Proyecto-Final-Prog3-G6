1cc7d2b5f1c255ec05b6b68078b8a13d3aa66f6b
Matiaslegon <matiaslegon@hotmail.com>
2026-06-13 11:53:15 -0300

    fix: arreglé estadisticas en modelo

--- backend/models/Libro.ts
+++ backend/models/Libro.ts
@@ -1,7 +1,6 @@
 import { Table, Column, Model, PrimaryKey, AutoIncrement, AllowNull } from 'sequelize-typescript';
 import { InterfaceLibro, EstadoLectura, ActualizarReseña } from '../interfaces/Libro-interface';
-import { DataTypes, Op } from 'sequelize';
-import { EstadisticasLibro } from '../interfaces/Estadistica-interface';
+import { DataTypes} from 'sequelize';
 
 @Table({
     tableName: 'libros',
@@ -105,27 +104,6 @@ export class Libro extends Model<InterfaceLibro> implements InterfaceLibro {
         return salida;
     }
 
-}
-
-export class Estadisticas { 
-    static async obtenerEstadisticas(): Promise <EstadisticasLibro> {
-    const totalLibros = await Libro.count();
-    const librosLeidos = await Libro.count({ where: { estado: EstadoLectura.Leido } });
-    const librosLeyendo = await Libro.count({ where: { estado: EstadoLectura.Leyendo } });
-    const librosPorLeer = await Libro.count({ where: { estado: EstadoLectura.PorLeer } });
-    const leidoReciente = await Libro.findOne({ where: { estado: { [Op.or]: [EstadoLectura.Leido, EstadoLectura.Leyendo] } }, order: [['updatedAt', 'DESC']] }); 
-    const terminadoReciente = await Libro.findOne({ where: { estado: EstadoLectura.Leido }, order: [['updatedAt', 'DESC']] }); //de Timestaps = T, sale de ahí el updateAT
-    const ultimoIncorporado = await Libro.findOne({ order: [['createdAt', 'DESC']] });
-    return {
-        TotalLibros: totalLibros,
-        LibrosLeidos: librosLeidos,
-        LibrosLeyendo: librosLeyendo,
-        LibrosPorLeer: librosPorLeer,
-        LeidoReciente: leidoReciente?.titulo || '-',
-        TerminadoReciente: terminadoReciente?.titulo || '-',
-        UltimoIncorporad: ultimoIncorporado?.titulo || '-'
-    };
-}
 }
 
 
