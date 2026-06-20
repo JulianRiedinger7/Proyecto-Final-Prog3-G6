492f4b8762aecdb3296b983a8afa7408796f6770
Julian Riedinger <julianriedinger27@gmail.com>
2026-06-20 15:37:48 -0300

    fix: mejorar formato y limpieza de código en controladores y utilidades

--- backend/controllers/estadisticas.controller.ts
+++ backend/controllers/estadisticas.controller.ts
@@ -1,15 +1,20 @@
-import {Response, Request, NextFunction} from 'express';
-import {Estadisticas} from '../utils/estadisticas.util';
-
+import { Response, Request, NextFunction } from "express";
+import { Estadisticas } from "../utils/estadisticas.util";
 
 export class EstadisticasController {
-    public getEstadisticas = async (req:Request, res: Response, next: NextFunction): Promise<Response | void> => {
-        let codigo: number;
-        let salida: object; 
-        try {codigo=200;
-            salida=await Estadisticas.obtenerEstadisticas();
-            return res.status(codigo).json(salida);}
-            catch (error) {
-                next(error);
-            }
-    }}
+  public getEstadisticas = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
+    let codigo: number;
+    let salida: object;
+    try {
+      codigo = 200;
+      salida = await Estadisticas.obtenerEstadisticas();
+      return res.status(codigo).json(salida);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
--- backend/utils/estadisticas.util.ts
+++ backend/utils/estadisticas.util.ts
@@ -1,10 +1,9 @@
 import { Libro } from "../models/libro.model";
-import { EstadisticasLibro } from '../interfaces/Estadistica.interface';
+import { EstadisticasLibro } from "../interfaces/Estadistica.interface";
 import { EstadoLectura } from "../interfaces/Libro.interface";
-import { Op } from "sequelize";
 
-export class Estadisticas { 
-    static async obtenerEstadisticas(): Promise <EstadisticasLibro> {
+export class Estadisticas {
+  static async obtenerEstadisticas(): Promise<EstadisticasLibro> {
     const totalLibros = await Libro.contar();
     const librosLeidos = await Libro.contarPorEstado(EstadoLectura.Leido);
     const librosLeyendo = await Libro.contarPorEstado(EstadoLectura.Leyendo);
@@ -13,13 +12,13 @@ export class Estadisticas {
     const terminadoReciente = await Libro.terminadoRecientemente();
     const ultimoIncorporado = await Libro.incorporadoRecientemente();
     return {
-        TotalLibros: totalLibros,
-        LibrosLeidos: librosLeidos,
-        LibrosLeyendo: librosLeyendo,
-        LibrosPorLeer: librosPorLeer,
-        LeidoReciente: leidoReciente || '-',
-        TerminadoReciente: terminadoReciente || '-',
-        UltimoIncorporad: ultimoIncorporado || '-'
+      TotalLibros: totalLibros,
+      LibrosLeidos: librosLeidos,
+      LibrosLeyendo: librosLeyendo,
+      LibrosPorLeer: librosPorLeer,
+      LeidoReciente: leidoReciente || "-",
+      TerminadoReciente: terminadoReciente || "-",
+      UltimoIncorporad: ultimoIncorporado || "-",
     };
-}
+  }
 }
