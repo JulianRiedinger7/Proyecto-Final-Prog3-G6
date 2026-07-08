ad40d5d786900a10c4683c016f3ad9cebf9b08dd
JDabus-dot <julietadabus@hotmail.com>
2026-07-08 17:29:24 -0300

    feat/agregocomponenteRutaProtegida

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { ReactNode } from "react";
+import { useAuth } from "../hooks/useAuth";
+
+export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const { token } = useAuth();
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+  return <>{children}</>;
+};
