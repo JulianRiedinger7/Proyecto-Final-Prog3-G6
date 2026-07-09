8e67e4261d298cde3688c6d3790ae350629ef7d6
Julian Riedinger <julianriedinger27@gmail.com>
2026-07-09 17:33:27 -0300

    feat: se agrego tipo de Libro

---
+++ frontend/src/types/Libro.type.ts
@@ -0,0 +1,12 @@
+export interface Libro {
+  id?: number;
+  titulo: string;
+  autor: string;
+  anio: number;
+  portada?: string;
+  estado?: "por leer" | "leyendo" | "leido";
+  puntaje?: number;
+  resenia?: string;
+  generoId?: number;
+  usuarioId?: number;
+}
