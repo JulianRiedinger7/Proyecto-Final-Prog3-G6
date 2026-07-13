e25a31c0c0371a221a810dc628ff155fab2089a1
Julian Riedinger <julianriedinger27@gmail.com>
2026-07-13 11:28:36 -0300

    feat: agregar componente StatsCard para mostrar estadísticas

--- frontend/src/components/common/statsCard.tsx
+++ frontend/src/components/common/statsCard.tsx
@@ -1,4 +1,9 @@
-export const StatsCard = ({ titulo, valor }: { titulo: string; valor: number }) => {
+interface StatsCardProps {
+  titulo: string;
+  valor: number;
+}
+
+export const StatsCard = ({ titulo, valor }: StatsCardProps) => {
   return (
     <div className="bg-secondary text-primary p-6 rounded-lg shadow-md m-4 w-64 font-semibold hover:bg-accent transition-colors duration-300  hover:text-secondary uppercase">
       <h3 className="text-xs mb-2">{titulo}</h3>
