57d0052b1fcec3ec2eef06a3ce23f88daca40486
Julian Riedinger <julianriedinger27@gmail.com>
2026-07-17 14:07:30 -0300

    feat: agregar filtrado por género en el Dashboard y mostrar géneros disponibles

--- frontend/src/pages/dashboard.page.tsx
+++ frontend/src/pages/dashboard.page.tsx
@@ -1,24 +1,31 @@
 import { obtenerEstadisticas, obtenerLibrosLeyendo } from "../services/estadisticas.service";
+import { generoService } from "../services/genero.service";
 import { useEffect, useState } from "react";
 import type { Estadisticas } from "../types/Estadisticas.type";
 import type { Libro } from "../types/Libro.type";
+import type { Genero } from "../types/Genero.type";
 import { StatsCard } from "../components/common/statsCard";
 import { BookCard } from "../components/common/bookCard";
 import { Link } from "react-router-dom";
+import { GeneroPill } from "../components/common/generoPill";
 
 export const DashboardPage = () => {
   const [estadisticas, setEstadisticas] = useState<Estadisticas | null>(null);
   const [libros, setLibros] = useState<Libro[]>([]);
+  const [generos, setGeneros] = useState<Genero[]>([]);
+  const [generoSeleccionado, setGeneroSeleccionado] = useState<number>(0);
 
   useEffect(() => {
     const fetchDatos = async () => {
       try {
-        const [estadisticasData, librosData] = await Promise.all([
+        const [estadisticasData, librosData, generosData] = await Promise.all([
           obtenerEstadisticas(),
           obtenerLibrosLeyendo(),
+          generoService.obtenerGeneros(),
         ]);
         setEstadisticas(estadisticasData);
         setLibros(librosData);
+        setGeneros(generosData);
       } catch (error) {
         console.error("Error al obtener los datos del dashboard:", error);
       }
@@ -26,6 +33,16 @@ export const DashboardPage = () => {
 
     fetchDatos();
   }, []);
+
+  const handleSeleccionado = (genero: Genero) => {
+    setGeneroSeleccionado((prevGeneroSeleccionado) =>
+      prevGeneroSeleccionado === genero.id ? 0 : genero.id,
+    );
+  };
+
+  const librosFiltrados = generoSeleccionado
+    ? libros.filter((libro) => libro.categoria?.id === generoSeleccionado)
+    : libros;
 
   return (
     <section>
@@ -51,9 +68,24 @@ export const DashboardPage = () => {
               Ver mejor calificados
             </Link>
           </div>
-          {/* Aca van las pills con los filtros de generos */}
+          <h4 className="text-lg font-bold mx-4">Filtrar por género:</h4>
+          <div className="flex flex-wrap gap-2 p-4">
+            {generos.length > 0 ? (
+              generos.map((genero) => (
+                <GeneroPill
+                  key={genero.id}
+                  genero={genero}
+                  activo={generoSeleccionado === genero.id}
+                  onClick={() => handleSeleccionado(genero)}
+                />
+              ))
+            ) : (
+              <p>Cargando géneros...</p>
+            )}
+          </div>
+
           <ul className="flex flex-col items-center md:flex-row md:flex-wrap justify-center">
-            {libros.map((libro) => (
+            {librosFiltrados.map((libro) => (
               <BookCard key={libro.id} libro={libro} />
             ))}
           </ul>
