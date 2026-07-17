3ee26624c09a8038b323c1161b85e7a728cbbe7e
Clara <vclarazivano@gmail.com>
2026-07-17 19:18:26 -0300

    Agrego layout, books, pages

---
+++ frontend/src/components/layout/MainLayout.tsx
@@ -0,0 +1,32 @@
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+
+interface Props{
+    children: React.ReactNode;
+}
+
+
+export default function MainLayout({children}:Props){
+
+return (
+
+<div className="flex">
+
+<Sidebar/>
+
+<div className="flex-1">
+
+<Navbar/>
+
+<main>
+{children}
+</main>
+
+</div>
+
+</div>
+
+)
+
+}
