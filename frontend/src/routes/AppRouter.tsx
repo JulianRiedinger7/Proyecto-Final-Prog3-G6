0ec15053591bc1722e75f58ff271b1d9e0110972
Clara <vclarazivano@gmail.com>
2026-07-26 13:06:54 -0300

    Agregue rutas biblioteca y añadir libro

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,52 +1,53 @@
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "../pages/login";
 import { GestionGenero } from "../pages/GestionGenero.pages";
+import { Biblioteca } from "../pages/Biblioteca";
+import { AnadirLibro } from "../pages/AnadirLibro";
 import { ProtectedRoute } from "../components/ProtectedRoute";
 import { MainLayout } from "../components/layout/MainLayout";
 import { Register } from "../pages/register";
 
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/generos" replace />} />
 
-export default function AppRouter(){
+      <Route path="/login" element={<Login />} />
 
-return (
+      <Route path="/registros" element={<Register />} />
 
+      <Route
+        path="/generos"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <GestionGenero />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
+      <Route
+        path="/biblioteca"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <Biblioteca />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
-<Routes>
-      
-        <Route 
-          path="/" 
-          element={<Navigate to="/generos" replace />} 
-        />
-
-        <Route 
-          path="/login" 
-          element={<Login />} 
-        />
-
-        <Route 
-          path="/registros" 
-          element={<Register />} 
-        />
-
-        
-        <Route 
-          path="/generos" 
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <GestionGenero />
-              </MainLayout>
-            </ProtectedRoute>
-          } 
-        />
-
-      
-
-
-</Routes>
-
-
-)
-
+      <Route
+        path="/anadir"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <AnadirLibro />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
