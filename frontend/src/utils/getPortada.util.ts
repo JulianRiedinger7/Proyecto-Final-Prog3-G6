e8257019f9505230a0a607a3d94b4a27ea237566
Julian Riedinger <julianriedinger27@gmail.com>
2026-07-26 17:29:17 -0300

    fix: se elimino getPortada util

--- frontend/src/utils/getPortada.util.ts
+++
@@ -1,8 +0,0 @@
-type PortadaSize = "S" | "M" | "L";
-
-export const getPortadaUrl = (portada: string | undefined, tamanio: PortadaSize): string => {
-  if (!portada) return "/book-cover-placeholder.png";
-  const idPortada = portada?.replace("OL", "");
-
-  return `https://covers.openlibrary.org/b/id/${idPortada}-${tamanio}.jpg`;
-};
